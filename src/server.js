5cd2ed0fd5aadfbc7815da087ab75b30682841d3
schmoop <sarora@arkaenergy.com>
2026-05-17 17:22:49 +0530

    updated server setup and removed heruko code

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
 'use strict';
 
+const express = require('express');
 const http = require('http');
-const nodeStatic = require('node-static');
 const path = require('path');
 
 function createServer(port) {
+  const app = express();
   const publicPath = path.join(__dirname, '../dist');
-  const fileServer = new nodeStatic.Server(publicPath);
-  
-  const app = http.createServer(function(req, res) {
-    fileServer.serve(req, res, function (err, result) {
-      if (err) {
-        console.error('Error serving ' + req.url + ' - ' + err.message);
-        if (!res.headersSent) {
-          res.writeHead(err.status, err.headers);
-          res.end();
-        }
-      }
-    });
+
+  // Serve static files from the /dist directory
+  app.use(express.static(publicPath));
+
+  // Catch-all route for Single Page App (optional, but good practice)
+  app.get('*', (req, res, next) => {
+    // If it's not a static file, we can either 404 or serve index.html
+    // For this app, let Express handle it.
+    next();
   });
 
-  app.listen(port, () => {
+  const server = http.createServer(app);
+
+  server.listen(port, () => {
     console.log('Server is listening on port ' + port);
+    console.log('Serving static files from: ' + publicPath);
   });
 
-  return app;
+  return server;
 }
 
 module.exports = { createServer };
